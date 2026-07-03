a0143c251a1f7ce9b2bc24791f922f78822847e2
PMCoding <pmcoding.com@gmail.com>
2026-07-03 09:33:57 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+    // ปล่อยผ่าน Request ไปยัง Network ปกติ
+});
